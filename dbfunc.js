a9a6decd38514483061c90a340a66a65c6e2f468
AdrianLayton <AdrianLayton@gmail.com>
2018-02-27 15:45:38 -0500

    Refac DB dunc syntax 3

--- dbfunc.js
+++ dbfunc.js
@@ -2,7 +2,7 @@ module.exports = {
 	makeParams: (userEntry, tableName) => {
 		const emailEntry = userEntry.email;
 
-		const table = tableName;
+		const tableName = tableName;
 		const params = {
 			Table: table,
 			Item: {
